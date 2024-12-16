3b633103c1739b2605161b2d6110f7ce542d81dc
gugugabijs <skyleradamsson@gmail.com>
2024-12-16 15:23:29 +0400

    css changes

--- src/Components/Boutiques/Boutiques.jsx
+++ src/Components/Boutiques/Boutiques.jsx
@@ -10,13 +10,13 @@ const Boutiques = () => {
         <div>
           <Link style={{ textDecoration: "none" }} to="/boutiques">
             <img src="https://partner.inloya.com/files/shop/branch/I82M41OU4R3133O048C9323MN81MSD4S.jpg" />
-            <h1>Lilac Winter Park</h1>
+            <h1 className="boutiques-text">Lilac Winter Park</h1>
           </Link>
         </div>
         <div>
           <Link style={{ textDecoration: "none" }} to="/boutiques">
             <img src="https://partner.inloya.com/files/shop/branch/Y2C487Y36DIAOOOYR20HPF62G45R8V68.jpg" />
-            <h1>Lilac Port Baku</h1>
+            <h1 className="boutiques-text">Lilac Port Baku</h1>
           </Link>
         </div>
       </div>
--- src/Components/HomeLinks/HomeLinks.jsx
+++ src/Components/HomeLinks/HomeLinks.jsx
@@ -12,12 +12,12 @@ const HomeLinks = () => {
             src="https://partner.inloya.com/files/shop/categories/Y1O3476CLIH5LTXSF75K519MX1N92S70.jpg"
             alt="bouqet"
           />
-          <h1>Flower Subscription</h1></Link>
+          <h1 className="home-link-text">Flower Subscription</h1></Link>
         </div>
         <div>
          <Link  style={{textDecoration:'none'}} to='/bouqets'>
             <img src='https://partner.inloya.com/files/shop/categories/9YM4WSHQ7G02234H9ITAQB1J0Y8S60CE.jpg' />
-            <h1>BOUQETS</h1>
+            <h1 className="home-link-text">BOUQETS</h1>
             </Link>
         </div>
       </div>
